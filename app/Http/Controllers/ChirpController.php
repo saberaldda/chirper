3b93df8227c1522a5439ecdcde2465bfd234c246
Saber <saberaldda@gmail.com>
2023-09-10 13:55:27 +0300

    Chirp MMC

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps');
+    }
+}
