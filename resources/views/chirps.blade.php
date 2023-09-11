b525df5b99b6fccf2f3fa772c5e7cb58fb4cc51f
Saber <saberaldda@gmail.com>
2023-09-11 14:55:58 +0300

    fixes

--- resources/views/chirps.blade.php
+++ resources/views/chirps.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8">
         <livewire:chirps.create />
 
         <livewire:chirps.list />
--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -48,10 +48,10 @@ new class extends Component
     }
 }; ?>
 
-<div>
-    <div class="mt-6 bg-white shadow-sm rounded-lg divide-y dark:bg-gray-500"> 
+<div class="py-10">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-3"> 
         @foreach ($chirps as $chirp)
-            <div class="p-6 flex space-x-2">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex space-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                 </svg>
