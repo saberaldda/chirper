4c7da9b2ea66f7444a0a5a4cfe704aef088a81fe
Saber <saberaldda@gmail.com>
2023-09-13 15:25:15 +0300

    pusher & Laravel Echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+var channel = Echo.private(`App.Models.User.${userID}`);
+channel.notification(function(data) {
+    console.log(data);
+    alert(data.message);
+    // alert(JSON.stringify(data));
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <script>
+            const userID = {{ Auth::id() }}
+        </script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
